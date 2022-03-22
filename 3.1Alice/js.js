ce4014314cf68d906079415246d688a9df81380f
mathocihara <matheusyukio.hocihara@gmail.com>
2022-03-22 01:02:23 -0300

    adição dos elementos estruturais e formação da história da Alice

---
+++ 3.1Alice/js.js
@@ -0,0 +1,146 @@
+
+      
+
+function FaseUm() {
+
+  var qntdTentativa = 1;
+  var tentativaAtual =  0;
+ 
+  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+        while (qntdTentativa >= tentativaAtual){
+              
+          if (resposta == 'a' || resposta == 'A') {
+              alert ('Acertou!')
+              window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase2.html";  
+              break
+      }   else if (  resposta != 'a' || resposta != 'A') {
+            if (qntdTentativa > 0) {
+              alert("Oh no")
+              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+              qntdTentativa --
+              } 
+              else {
+                  alert('....')
+                  window.location.href= 'Fim.html'
+                  break
+
+              } 
+            }
+          }
+        }
+function FaseDois() {
+
+  var qntdTentativa = 1;
+  var tentativaAtual = 0;
+ 
+  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+        while (qntdTentativa >= tentativaAtual){
+              
+          if (resposta == 'a' || resposta == 'A') {
+              alert ('Acertou!')
+              window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase3.html";  
+              break
+      }   else if (  resposta != 'a' || resposta != 'A') {
+            if (qntdTentativa > 0) {
+              alert("Oh no")
+              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+              qntdTentativa --
+              } 
+              else {
+                  alert('....')
+                  window.location.href= 'Fim.html'
+                  break
+
+              } 
+            }
+          }
+        }
+function FaseTres() {
+
+          var qntdTentativa = 1;
+          var tentativaAtual = 0;
+         
+          var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                while (qntdTentativa >= tentativaAtual){
+                      
+                  if (resposta == 'a' || resposta == 'A') {
+                      alert ('Acertou!')
+                      window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase4.html";  
+                      break
+              }   else if (  resposta != 'a' || resposta != 'A') {
+                    if (qntdTentativa > 0) {
+                      alert("Oh no")
+                      var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                      qntdTentativa --
+                      } 
+                      else {
+                          alert('....')
+                          window.location.href= 'Fim.html'
+                          break
+        
+                      } 
+                    }
+                  }
+        }
+function FaseQuatro() {
+
+                  var qntdTentativa = 1;
+                  var tentativaAtual = 0;
+                 
+                  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                        while (qntdTentativa >= tentativaAtual){
+                              
+                          if (resposta == 'a' || resposta == 'A') {
+                              alert ('Acertou!')
+                              window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase5.html";  
+                              break
+                      }   else if (  resposta != 'a' || resposta != 'A') {
+                            if (qntdTentativa > 0) {
+                              alert("Oh no")
+                              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                              qntdTentativa --
+                              } 
+                              else {
+                                  alert('....')
+                                  window.location.href= 'Fim.html'
+                                  break
+                
+                              } 
+                            }
+                          }
+        }
+function FaseCinco() {
+
+                          var qntdTentativa = 1;
+                          var tentativaAtual = 0;
+                         
+                          var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                                while (qntdTentativa >= tentativaAtual){
+                                      
+                                  if (resposta == 'a' || resposta == 'A') {
+                                      alert ('Acertou!')
+                                      window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase6.html";  
+                                      break
+                              }   else if (  resposta != 'a' || resposta != 'A') {
+                                    if (qntdTentativa > 0) {
+                                      alert("Oh no")
+                                      var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                                      qntdTentativa --
+                                      } 
+                                      else {
+                                          alert('....')
+                                          window.location.href= 'Fim.html'
+                                          break
+                        
+                                      } 
+                                    }
+                                  }
+        }
+        
+        
+        
+
+
+        
+      
+    
