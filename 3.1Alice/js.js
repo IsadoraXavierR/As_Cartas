72ab78dabacffedd76182075811b8f61f3973d3c
mathocihara <matheusyukio.hocihara@gmail.com>
2022-03-22 02:01:57 -0300

    Restruturei meu function para melhor desenvolvimento e finalizei a hsitoria da Alice

--- 3.1Alice/js.js
+++ 3.1Alice/js.js
@@ -6,21 +6,21 @@ function FaseUm() {
   var qntdTentativa = 1;
   var tentativaAtual =  0;
  
-  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+  var resposta = prompt('Escolha entre as opções "A"; "B"; "C"' );     
         while (qntdTentativa >= tentativaAtual){
               
-          if (resposta == 'a' || resposta == 'A') {
+          if (resposta == 'b' || resposta == 'B') {
               alert ('Acertou!')
               window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase2.html";  
               break
-      }   else if (  resposta != 'a' || resposta != 'A') {
+      }   else if (  resposta != 'b' || resposta != 'B') {
             if (qntdTentativa > 0) {
-              alert("Oh no")
-              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+              alert("Foi flagrada!! Tente novamente com cautela.")
+              var resposta = prompt('Escolha entre as opções "A"; "B"; "C"' );     
               qntdTentativa --
               } 
               else {
-                  alert('....')
+                  alert('Os seguranças te pegaram, game over.')
                   window.location.href= 'Fim.html'
                   break
 
@@ -33,21 +33,21 @@ function FaseDois() {
   var qntdTentativa = 1;
   var tentativaAtual = 0;
  
-  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+  var resposta = prompt('Escolha entre as opções "A"; "B"; "C" ou "D"' );     
         while (qntdTentativa >= tentativaAtual){
               
-          if (resposta == 'a' || resposta == 'A') {
+          if (resposta == 'c' || resposta == 'C') {
               alert ('Acertou!')
               window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase3.html";  
               break
-      }   else if (  resposta != 'a' || resposta != 'A') {
+      }   else if (  resposta != 'c' || resposta != 'C') {
             if (qntdTentativa > 0) {
-              alert("Oh no")
-              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+              alert("Estava perto demais da briga, saiu machucada. Tente novamente")
+              var resposta = prompt('Escolha entre as opções "A"; "B"; "C" ou "D"' );     
               qntdTentativa --
               } 
               else {
-                  alert('....')
+                  alert('Seguranças te flagraram, Game Over')
                   window.location.href= 'Fim.html'
                   break
 
@@ -63,18 +63,18 @@ function FaseTres() {
           var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
                 while (qntdTentativa >= tentativaAtual){
                       
-                  if (resposta == 'a' || resposta == 'A') {
+                  if (resposta == 'b' || resposta == 'B') {
                       alert ('Acertou!')
                       window.location.href= "http://127.0.0.1:5500/3.1Alice/Fase4.html";  
                       break
-              }   else if (  resposta != 'a' || resposta != 'A') {
+              }   else if (  resposta != 'b' || resposta != 'B') {
                     if (qntdTentativa > 0) {
-                      alert("Oh no")
+                      alert("Os policiais estão desconfiando, seja rápida!")
                       var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
                       qntdTentativa --
                       } 
                       else {
-                          alert('....')
+                          alert('Foi questionada pelos policiais e te descobriram, game over.')
                           window.location.href= 'Fim.html'
                           break
         
@@ -87,7 +87,7 @@ function FaseQuatro() {
                   var qntdTentativa = 1;
                   var tentativaAtual = 0;
                  
-                  var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                  var resposta = prompt('Escolha entre as opções "A"; "B"; "C"' );     
                         while (qntdTentativa >= tentativaAtual){
                               
                           if (resposta == 'a' || resposta == 'A') {
@@ -96,12 +96,12 @@ function FaseQuatro() {
                               break
                       }   else if (  resposta != 'a' || resposta != 'A') {
                             if (qntdTentativa > 0) {
-                              alert("Oh no")
-                              var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
+                              alert("Não é a melhor opção.")
+                              var resposta = prompt('Escolha entre as opções "A"; "B"; "C"' );     
                               qntdTentativa --
                               } 
                               else {
-                                  alert('....')
+                                  alert('Esperou muito do "estranho", game over.')
                                   window.location.href= 'Fim.html'
                                   break
                 
@@ -123,12 +123,12 @@ function FaseCinco() {
                                       break
                               }   else if (  resposta != 'a' || resposta != 'A') {
                                     if (qntdTentativa > 0) {
-                                      alert("Oh no")
+                                      alert("Não o atrasou, arremesse algo outro utensilio.")
                                       var resposta = prompt('Escolha entre as opções "A"; "B";"C"' );     
                                       qntdTentativa --
                                       } 
                                       else {
-                                          alert('....')
+                                          alert('Que pena, você não sairá mais. Game Over.')
                                           window.location.href= 'Fim.html'
                                           break
                         
