7c5451bec4f2aaf837319ba3dfdfd1b89e2e0468
mathocihara <100290301+mathocihara@users.noreply.github.com>
2022-03-26 00:01:06 -0300

    reformando erro

--- 2.personagens/js.js
+++ 2.personagens/js.js
@@ -1,10 +1,10 @@
 function Alice(){
-    location.href= "./3.1Alice/Fase1.html"  
+    location.href= "../3.1Alice/Fase1.html"  
 }
 function Lucas(){
-    location.href= './3.2Lucas/fase1.html'
+    location.href= '../3.2Lucas/fase1.html'
 }
 function Marcos(){
-    location.href= './3.3Marcos/fase1.html'
+    location.href= '../3.3Marcos/fase1.html'
 
 }
