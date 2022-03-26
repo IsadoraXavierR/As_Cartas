40f65585da0651a71beed6901ab89949549b9de1
mathocihara <matheusyukio.hocihara@gmail.com>
2022-03-25 22:01:48 -0300

    Estilizei as paginas da história do lucas

--- 3.2Lucas/js.js
+++ 3.2Lucas/js.js
@@ -4,7 +4,7 @@ function FaseUm(){
 
         if (resposta == 'b' || resposta == 'B' || resposta == 'a' || resposta == 'A' || resposta == 'c' || resposta == 'C' ) {
             alert ('Belo drink')
-                window.location.href= '../final/Fim.html'  
+                window.location.href= '../3.2Lucas/fase2.html'  
         }   else{
             alert ('Belo drink')
         }}
@@ -40,47 +40,19 @@ function FaseDois(){
         }
 
 
-// function FaseTres(){
-    
-//     var qntdTentativa = 1
-//     var tentativaAtual =  0
-   
-//           while (qntdTentativa >= tentativaAtual){
-  
-//             var resposta = prompt('Escolha entre as opções "A"; "B"; "C"' );  
-  
-//             if (resposta == 'D' || resposta == 'd') {
-//                 alert ('Perfeito, já acionaram os policiais e funcionários do trem;')
-//                 window.location.href= "../3.2Lucas/Fase2.html"  
-//                 break
-//         }   else if (  resposta != 'D' || resposta != 'd') {
-//               if (qntdTentativa > 0) {
-//                 alert(" CUIDADO COM A BRIGA!!")   
-//                 qntdTentativa --
-//                 } 
-//                 else {
-//                     alert('Alguem bateu em você e causou desmaio, ficou dormindo em todo destino')
-//                     alert('Game Over!!')
-//                     window.location.href= 'Fim.html'
-//                     break
-  
-//                 } 
-//               }
-//             }
-//           }
-
 function FaseQuatro(){
     
             var resposta = prompt('Escolha entre as opções "A"; "B"' );  
         
-            if (resposta == 'a' || resposta == 'A' || resposta == 'a' || resposta == 'A' || resposta == 'c' || resposta == 'C' ) {
+            if (resposta == 'B' || resposta == 'b' ) {
                 alert ('Continue, mas com cuidado')
                 window.location.href= "../3.2Lucas/fase5.html"  
-            }   else{
+            }   else {
                 alert ('Infelizmente algo de terrivel aconteceu, você se arrependeu para sempre...')
                 window.location.href= '../final/Fim.html'  
         
             }}
+
 
 function FaseCinco(){
     
