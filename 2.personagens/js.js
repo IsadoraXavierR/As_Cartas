7d2ff3d4d129da626cf33a16fcc048ebd3bb0e54
mathocihara <matheusyukio.hocihara@gmail.com>
2022-03-25 23:51:25 -0300

    reformando erro

--- 2.personagens/js.js
+++ 2.personagens/js.js
@@ -1,10 +1,10 @@
 function Alice(){
-    window.location.href= "./3.1Alice/Fase1.html"  
+    location.href= "./3.1Alice/Fase1.html"  
 }
 function Lucas(){
-    window.location.href= './3.2Lucas/fase1.html'
+    location.href= './3.2Lucas/fase1.html'
 }
 function Marcos(){
-    window.location.href= './3.3Marcos/fase1.html'
+    location.href= './3.3Marcos/fase1.html'
 
 }
